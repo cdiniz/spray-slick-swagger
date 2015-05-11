af90cf6febfceb7bee28265a1db7fc89071d415e
Cláudio Diniz <cdiniz@pminds.pt>
2015-05-11 14:00:13 +0100

    Test for get supplier by id that should return an empty sequence

--- src/test/scala/persistence/dal/SuppliersDAATest.scala
+++ src/test/scala/persistence/dal/SuppliersDAATest.scala
@@ -19,13 +19,14 @@ class SuppliersDAATest extends FunSuite with AbstractPersistenceTest with Before
   val modules = new Modules {
   }
 
-
-  test("SuppliersActor: Test Suppliers Actor") {
+  test("SuppliersActor: Testing Suppliers Actor") {
     Await.result((modules.suppliersDAA ? CreateTables).mapTo[Future[Unit]],5.seconds)
     val numberOfEntities : Int = Await.result((modules.suppliersDAA ? Save(Supplier(None,"sup","desc"))).mapTo[Future[Int]].flatMap(x => x),5.seconds)
     assert (numberOfEntities == 1)
     val supplier : Seq[Supplier] = Await.result((modules.suppliersDAA ? GetSupplierById(1)).mapTo[Future[Seq[Supplier]]].flatMap(x => x),5.seconds)
     assert (supplier.length == 1 &&  supplier.head.name.compareTo("sup") == 0)
+    val empty : Seq[Supplier] = Await.result((modules.suppliersDAA ? GetSupplierById(2)).mapTo[Future[Seq[Supplier]]].flatMap(x => x),5.seconds)
+    assert (empty.length == 0)
   }
 
   override def afterAll: Unit ={

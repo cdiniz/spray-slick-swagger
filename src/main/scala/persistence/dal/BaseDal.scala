2708413b32da35d1153599ebeb9fba26c8e4930f
Cláudio Diniz <claudio.diniz@enear.co>
2016-02-23 11:13:15 +0000

    Update baseDal

--- src/main/scala/persistence/dal/BaseDal.scala
+++ src/main/scala/persistence/dal/BaseDal.scala
@@ -1,38 +1,77 @@
 package persistence.dal
 
 import slick.driver.H2Driver.api._
+import slick.lifted.CanBeQueryCondition
 import utils.{DbModule, Profile}
 import scala.concurrent.Future
-
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait BaseEntity {
   val id : Long
+  def isValid : Boolean = true
 }
-
 
 abstract class BaseTable[T](tag: Tag, name: String) extends Table[T](tag, name) {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
 }
 
+trait BaseDAOI[T,A] {
+  def insert(row : A): Future[Long]
+  def insert(rows : Seq[A]): Future[Seq[Long]]
+  def update(row : A): Future[Int]
+  def update(rows : Seq[A]): Future[Unit]
+  def findById(id : Long): Future[Option[A]]
+  def findByFilter[C : CanBeQueryCondition](f: (T) => C): Future[Seq[A]]
+  def deleteById(id : Long): Future[Int]
+  def deleteById(ids : Seq[Long]): Future[Int]
+  def deleteByFilter[C : CanBeQueryCondition](f:  (T) => C): Future[Int]
+}
+
+
 trait BaseDal[T <: BaseTable[A], A <: BaseEntity] extends Profile with DbModule {
+
   import profile.api._
+
   protected val tableQ: TableQuery[T]
 
-  def insert(row : A): Future[Long] ={
-    db.run((tableQ returning tableQ.map(_.id) += row))
+  def insert(row: A): Future[Long] = {
+    insert(Seq(row)).map(_.head)
   }
 
-  def update(row : A): Future[Int] = {
-    db.run(tableQ.filter(_.id === row.id).update(row))
+  def insert(rows: Seq[A]): Future[Seq[Long]] = {
+    db.run(tableQ returning tableQ.map(_.id) ++= rows.filter(_.isValid))
   }
 
-  def findById(id : Long): Future[Option[A]] = {
+  def update(row: A): Future[Int] = {
+    if (row.isValid)
+      db.run(tableQ.filter(_.id === row.id).update(row))
+    else
+      Future {
+        0
+      }
+  }
+
+  def update(rows: Seq[A]): Future[Unit] = {
+    db.run(DBIO.seq((rows.filter(_.isValid).map(r => tableQ.filter(_.id === r.id).update(r))): _*))
+  }
+
+  def findById(id: Long): Future[Option[A]] = {
     db.run(tableQ.filter(_.id === id).result.headOption)
   }
 
-  def delete(id : Long): Future[Int] = {
-    db.run(tableQ.filter(_.id === id).delete)
+  def findByFilter[C: CanBeQueryCondition](f: (T) => C): Future[Seq[A]] = {
+    db.run(tableQ.withFilter(f).result)
   }
 
-}
+  def deleteById(id: Long): Future[Int] = {
+    deleteById(Seq(id))
+  }
 
+  def deleteById(ids: Seq[Long]): Future[Int] = {
+    db.run(tableQ.filter(_.id.inSet(ids)).delete)
+  }
+
+  def deleteByFilter[C: CanBeQueryCondition](f: (T) => C): Future[Int] = {
+    db.run(tableQ.withFilter(f).delete)
+  }
+}
